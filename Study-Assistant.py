5214a0fc0d2761cb5151066cf2ad16a4a4d6ec16
Shrinivas Bhong <shrinivasbhong2428@gmail.com>
2025-06-18 16:04:53 +0530

    removing pdf2image

--- Study-Assistant.py
+++ Study-Assistant.py
@@ -11,7 +11,6 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import AIMessage, HumanMessage
 import pdfplumber
-from pdf2image import convert_from_path
 import pytesseract
 
 # ===============================
